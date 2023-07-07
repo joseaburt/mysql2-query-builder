34a93ada13e9bc5aabd73b6066ce730534cd8983
JoseAburto <jose.aburto@delinernet.com>
2023-07-07 22:19:56 +0200

    ref: adds logs

--- src/connection-decorator.ts
+++ src/connection-decorator.ts
@@ -2,6 +2,7 @@ import SelectQueryBuilder from './select-query';
 import mysql, { ResultSetHeader } from 'mysql2/promise';
 
 export default class ConnectionDecorator {
+  public static debug: boolean = false;
   constructor(private connection: mysql.Connection) {}
 
   public getRawConnection(): mysql.Connection {
@@ -9,7 +10,10 @@ export default class ConnectionDecorator {
   }
 
   public async closeConnection(): Promise<void> {
-    return await this.connection.end();
+    await this.connection.end();
+    if (ConnectionDecorator.debug) {
+      console.log('Connection closed');
+    }
   }
 
   public select(...columns: string[]): Pick<SelectQueryBuilder, 'from'> {
@@ -23,12 +27,20 @@ export default class ConnectionDecorator {
 
   public async update<T, P extends T = T>(table: string, payload: P, where: string, values: any[]): Promise<boolean> {
     const resultSet = await this.connection.query(`UPDATE ${table} SET ? ${where}`, [payload, values]);
+    if (ConnectionDecorator.debug) {
+      console.log(`UPDATE ${table} SET ? ${where}`);
+      console.log(resultSet);
+    }
     const result = resultSet[0] as ResultSetHeader;
     return !!result.changedRows;
   }
 
   public async insert<T, P extends T = T>(table: string, payload: P): Promise<number> {
     const res = await this.connection.query(`INSERT INTO ${table} SET ?`, payload);
+    if (ConnectionDecorator.debug) {
+      console.log(`INSERT INTO ${table} SET ?`);
+      console.log(res);
+    }
     const result = res[0] as ResultSetHeader;
     return result.insertId;
   }
@@ -36,6 +48,10 @@ export default class ConnectionDecorator {
   public async delete(table: string, where: string, values: any[]): Promise<boolean> {
     const res = await this.connection.query(`DELETE FROM ${table} ${where}`, values);
     const result = res[0] as ResultSetHeader;
+    if (ConnectionDecorator.debug) {
+      console.log(`DELETE FROM ${table} ${where}`);
+      console.log(res);
+    }
     return !!result.affectedRows;
   }
 }
