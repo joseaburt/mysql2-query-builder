f57f699a8b75aa5c09b443139c1fdabe7e2b3596
JoseAburto <jose.aburto@delinernet.com>
2023-07-07 13:01:46 +0200

    fix: where condition

--- src/select-query.ts
+++ src/select-query.ts
@@ -1,13 +1,11 @@
 import ConnectionDecorator from './connection-decorator';
 
 export default class SelectQueryBuilder {
-  private isWhereOn: boolean;
   private sql: string = '';
   private connection: ConnectionDecorator | undefined;
 
   private constructor(sql: string, connection?: ConnectionDecorator) {
     this.sql = sql;
-    this.isWhereOn = false;
     this.connection = connection;
   }
 
@@ -34,19 +32,17 @@ export default class SelectQueryBuilder {
     return this;
   }
 
-  public on(condition: string): Pick<SelectQueryBuilder, 'where' | 'get'> {
+  public on(condition: string): Pick<SelectQueryBuilder, 'where' | 'get' | 'execute'> {
     this.sql += 'ON ' + condition + ' ';
     return this;
   }
 
   public where(...condition: string[]): Pick<SelectQueryBuilder, 'get' | 'execute'> {
-    this.isWhereOn = true;
     this.sql += 'WHERE ' + condition.join(' AND ');
     return this;
   }
 
   public async execute<R = any>(args?: any[]): Promise<R[]> {
-    if ((!args || args.length === 0) && this.isWhereOn) throw new Error('ArgumentsForWhereConditionNeededError');
     if (!this.connection) throw new Error('ConnectionNotDefined');
     return this.connection.execute(this.sql, args);
   }
